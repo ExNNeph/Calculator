2cf81e3a3d582e97ac746ea1ff908cfde927d754
Alexander Kramer <alexander.kramer@mni.thm.de>
2023-11-06 07:15:26 +0100

    Calculator App

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//This is my Calculator Code
+/*
+function calculate() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var operator = document.getElementById("operator").value;
+    var result;
+
+    if (operator == "add") {
+        result = parseInt(num1) + parseInt(num2);
+    } else if (operator == "sub") {
+        result = parseInt(num1) - parseInt(num2);
+    } else if (operator == "mul") { 
+        result = parseInt(num1) * parseInt(num2);
+    } else if (operator == "div") {
+        result = parseInt(num1) / parseInt(num2);
+    } else if (operation == "choose") {
+        result = "Error";
+    }
+
+    document.getElementById("result").innerHTML = result;
+}
+
+id=result;
+*/
